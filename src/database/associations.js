2cc3f74e85a065fb90d4a49243402a716cb76c93
Jared <u202020239@upc.edu.pe>
2024-12-09 11:28:38 -0500

    Feat: Start make models

--- src/database/associations.js
+++ src/database/associations.js
@@ -2,6 +2,9 @@ const Order = require("../models/Oder");
 const OrderDetail = require("../models/OrderDetail");
 const Product = require("../models/Product");
 const Inventory = require("../models/Inventory");
+const User = require("../models/User")
+const Review = require("../models/Review")
+const Store = require("../models/Store")
 
 Order.hasMany(OrderDetail) 
 OrderDetail.belongsTo(Order)
@@ -11,3 +14,12 @@ Product.hasMany(OrderDetail)
 
 Product.hasMany(Inventory)
 Inventory.belongsTo(Product)
+
+Review.hasMany(User)
+User.belongsTo(Review)
+
+Review.hasMany(Store)
+Store.belongsTo(Review)
+
+Store.hasMany(User)
+User.belongsTo(Store)
---
+++ src/models/User.js
@@ -0,0 +1,23 @@
+const {Model, DataTypes} = require("sequelize");
+const sequelize = require("../database/db");
+
+class User extends Model {}
+User.init(
+    {
+        name:{
+            type: DataTypes.STRING
+        },
+        email:{
+            type: DataTypes.STRING
+        },
+        passwaord:{
+            type: DataTypes.STRING
+        }
+    },{
+        sequelize,
+        modelName: "user",
+        timestamps: false
+    }
+)
+
+module.exports = User;
